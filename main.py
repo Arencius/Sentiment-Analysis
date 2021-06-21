75540b965b64502124c425a08ec57dcb60642f7a
Karol Krawczyk <karol.krawczyk1@edu.uekat.pl>
2021-06-21 14:29:31 +0200

    improved the user interface window

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ import logging
 from keras.models import load_model
 from gensim.models import Word2Vec
 from src.gui.gui_window import App
-from src.network.model import MAX_LENGTH
 
 
 if __name__ == '__main__':
+    MAX_LENGTH = 50
+
     # TODO fix this
     logging.disable(logging.ERROR)
     os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
--- src/gui/gui_window.py
+++ src/gui/gui_window.py
@@ -6,29 +6,48 @@ class App:
 
     def __init__(self, model, vocabulary, max_length):
         self.root = tk.Tk()
-        self.root.geometry('500x500')
+        self.root.geometry('500x375')
+        self.root.title('Sentiment analysis')
 
         self.model = model
         self.vocab = vocabulary
         self.max_length = max_length
+        self.font = ('Roboto', 12)
 
-        self.title_label = tk.Label(self.root, text = 'Type your sentence: ')
-        self.title_label.pack()
+        self.title_label = tk.Label(self.root, text = 'Type your sentence: ', font = ('Roboto', 14))
+        self.title_label.pack(pady = 5)
 
-        self.text_entry = tk.Entry(self.root)
-        self.text_entry.pack()
+        # text area
+        self.text_entry = tk.Text(self.root, font = self.font, width = 45, height = 10)
+        self.text_entry.pack(pady = 10, padx = 10)
 
-        self.submit_button = tk.Button(self.root, text = 'Check sentiment', command = self.display_sentiment)
-        self.submit_button.pack()
+        # receives the input and predicts the sentiment using pretrained LSTM model
+        self.submit_button = tk.Button(self.root, text = 'Check sentiment', font = self.font,
+                                       command = self.display_sentiment)
+        self.submit_button.pack(pady=5)
 
-        self.result_label = tk.Label(self.root)
-        self.result_label.pack()
+        # clears the text area
+        self.clear_button = tk.Button(self.root, text = 'Clear', font = self.font,
+                                      command = self.clear)
+        self.clear_button.pack(pady=5)
+
+        # displays the predicted sentiment
+        self.result_label = tk.Label(self.root, font = self.font)
+        self.result_label.pack(pady=5)
 
         self.root.mainloop()
 
+    def clear(self):
+        """
+        Clears the text area, so the user can type in new sentence
+        """
+        self.text_entry.delete(1.0, tk.END)
+
     def display_sentiment(self):
-        sentence = self.text_entry.get()
-        self.text_entry.delete(0, 'end')
+        """
+        Receives the user input from the text area, predicts its sentiment and displays it
+        """
+        sentence = self.text_entry.get(1.0, tk.END)
 
         predicted = predict_sentiment(self.model, sentence, App.SENTIMENTS, self.max_length, self.vocab)
         self.result_label['text'] = predicted
